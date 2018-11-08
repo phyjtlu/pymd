a0cb6b9c9cf1ac41a85e426d981e663557e2593f
aperture <i@oau.io>
2018-11-08 16:04:16 +0800

    Update a test example

--- drivers/lammps/test.py
+++ drivers/lammps/test.py
@@ -1,8 +1,18 @@
-#using LAMMPS as a driver
+# using LAMMPS as a driver
 from lammps import lammps
 import numpy as N
 
 lmp = lammps(label="test", infile="in.test")
 lmp.start()
-print lmp.f0
+
+deltaq0 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+deltaq1 = [0, 0, 0, 0, 0, 0, 1.4, -1.4, 0]
+
+# f0=lmp.f0
+f0 = lmp.absforce(deltaq0)
+deltaf0 = lmp.force(deltaq0)
+f1 = lmp.absforce(deltaq1)
+deltaf1 = lmp.force(deltaq1)
+
+print f0, '\n', deltaf0, '\n', f1, '\n', deltaf1
 lmp.quit()
