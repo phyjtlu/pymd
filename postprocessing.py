61c0d7f65e15a59186a14d38420c7ff7509b1c1a
oauio <i@oau.io>
2020-04-07 23:23:25 +0800

    Update average heat flux calculation.

--- postprocessing.py
+++ postprocessing.py
@@ -2,16 +2,22 @@ import glob
 import numpy as np
 #from matplotlib import pyplot as plt
 
-def CalHF(temp,dlist=1):
+#calculate average heat flux
+def CalHF(dlist=1):
     print("Calculate heat flux.")
-    temperture=temp
+    #temperture=temp
+    for filename in glob.glob('./kappa.*.bath0.run0.dat'):
+       with open(filename, 'r') as f:
+            for line in f: 
+                temperture=float(line.split()[1])
+
     dlist=list(range(dlist))
     times=int(len(glob.glob('./kappa.*.bath*.run*.dat'))/2)
     kb=np.empty([2,times])
 
     for i in list(range(2)):
         for j in list(range(times)):
-            kappafile="./kappa."+str(temperture)+".bath"+str(i)+".run"+str(j)+".dat"
+            kappafile="./kappa."+str(int(temperture))+".bath"+str(i)+".run"+str(j)+".dat"
             for files in glob.glob(kappafile): 
                 with open(files, 'r') as f: 
                     for line in f: 
@@ -29,29 +35,37 @@ def CalHF(temp,dlist=1):
         f.write("Temperture\t"+str(temperture)+"\n"+"Bath0\t"+str(balancekb[0])+"\n"+"Bath1\t"+str(balancekb[1])+"\n"+"HeatFlux\t"+str(heatflux)+"\n"+"\n")
 
 #calculate thermal conductance
-def CalTC(deltaT,temp,dlist=0):
+def CalTC(delta,dlist=0):
     print("Calculate thermal conductance.")
-    deltaT=deltaT
-    temperture=temp
+    delta=delta
+    #temperture=temp
+    for filename in glob.glob('./kappa.*.bath0.run0.dat'):
+       with open(filename, 'r') as f:
+            for line in f: 
+                temperture=float(line.split()[1])
     dlist=list(range(dlist))
     times=int(len(glob.glob('./kappa.*.bath*.run*.dat'))/2)
     kb=np.empty([2,times])
 
     for i in list(range(2)):
         for j in list(range(times)):
-            kappafile="./kappa."+str(temperture)+".bath"+str(i)+".run"+str(j)+".dat"
+            kappafile="./kappa."+str(int(temperture))+".bath"+str(i)+".run"+str(j)+".dat"
             for files in glob.glob(kappafile): 
                 with open(files, 'r') as f: 
                     for line in f: 
                         kb[i][j]=line.split()[2]
 #                        temperture=float(line.split()[1])
-    kappa=(kb[0]-kb[1])/2/(deltaT)
+    kappa=(kb[0]-kb[1])/2/(delta*temperture)
     kappa=np.delete(kappa,dlist)
-    for i in range(len(kappa)):
-        kappa[i]=np.mean(kappa[0:i+1])
+    #for i in range(len(kappa)):
+    #    kappa[i]=np.mean(kappa[0:i+1])
 
     with open('thermalconductance.'+str(int(temperture))+'.dat','w') as f:
         f.write("Temperture\t"+str(temperture)+"\n"+"ThermalConductance\t"+str(kappa)+"\n"+"Mean\t"+str(np.mean(kappa))+"\n"+"StandardDeviation\t"+str(np.std(kappa))+"\n")
+
+#calculate average powerspectra
+def CalPS():
+    pass
 
 #powerspectra = np.loadtxt('power2.300.run1.dat')
 #
